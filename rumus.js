e6f9757eb5a9554010a34fed12f6c422e12f3ff3
MacBook <macbook@MacBook-Pro.local>
2025-12-09 20:08:42 +0700

    initial commit

---
+++ rumus.js
@@ -0,0 +1,16 @@
+// rumus.js
+export function kalkulator(angka1, angka2, operator) {
+    switch (operator) {
+        case "+":
+            return angka1 + angka2;
+        case "-":
+            return angka1 - angka2;
+        case "*":
+            return angka1 * angka2;
+        case "/":
+            if (angka2 === 0) return "Tidak bisa membagi dengan 0!";
+            return angka1 / angka2;
+        default:
+            return "Operator tidak valid!";
+    }
+}
